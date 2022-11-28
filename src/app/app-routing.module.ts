433ce4e495993576c1d5eddf0325074935a42c8b
ANUMOL V N <anumolvn123@gmail.com>
2022-11-28 11:32:47 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Home', component: HomeComponent },
+{ path: 'AboutUs', component: AboutUsComponent },
+{ path: 'ContactUs', component: ContactUsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
